522c4f6d38e54c2b136a99c4ce594883f9f745b7
Nic Grundhoffer <ngrundhoffer@cnm.edu>
2024-04-30 21:05:25 -0600

    sorting through pokemon results

--- index.js
+++ index.js
@@ -3,11 +3,20 @@ const axios = require('axios')
 let pokeList = []
 
 async function loadPokemon () {
-    const pokeList = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=1400')
+    pokeList = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=1400')
         .then(function (response) {
-            return response.data
+         return response.data.results
         })
-    console.log(pokeList)
+        .catch(function (error){
+            //handle error
+            console.log(error)
+        })
+
 }
 
 loadPokemon()
+
+
+pokeList.filter((pokemon) => {
+
+})
